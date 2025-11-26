377139caf68468d390c027c75936006095264481
block-mo <block.moktan@outlook.com>
2025-11-25 22:18:19 -0700

    style(backend): format code with Prettier

--- backend/tests/telemetry.test.js
+++ backend/tests/telemetry.test.js
@@ -1,10 +1,10 @@
 // Import supertest using ES modules
-import request from 'supertest'
+import request from "supertest";
 
 // Import express using ESM
-import express from 'express'
+import express from "express";
 
 // Example dummy Jest test
-test('dummy test placeholder', () => {
-  expect(true).toBe(true)
-})
+test("dummy test placeholder", () => {
+  expect(true).toBe(true);
+});
